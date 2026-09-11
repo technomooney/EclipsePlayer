6bfc9bb2feb6ae8a3dc529e0ba91f24be2083c6d
Marty M <github@moontronic.com>
2026-09-11 12:27:57 -0600

    Added new axis maps based on the current tcode specs found on TMax's Discord.

--- EclipsePlayer.Funscript/Model/AxisName.cs
+++ EclipsePlayer.Funscript/Model/AxisName.cs
@@ -11,6 +11,11 @@ public readonly record struct AxisName(string Name)
     public static AxisName Roll => new("roll");
     public static AxisName Pitch => new("pitch");
     public static AxisName Suck => new("suck");
+    public static AxisName Vib0 => new("vib0");
+    public static AxisName Vib1 => new("vib1");
+    public static AxisName Vib2 => new("vib2");
+    public static AxisName Valve => new("valve");
+    public static AxisName Lube => new("lube");
 
     public static AxisName From(string raw)
     {
@@ -25,6 +30,12 @@ public readonly record struct AxisName(string Name)
             "r1"=>"roll",
             "r2"=>"pitch",
             "a1"=>"suck",
+            "v0"=>"vib0",
+            "v1"=>"vib1",
+            "v2"=>"vib2",
+            "a0"=>"valve",
+            "a2"=>"lube",
+
             _=>processed,
         };
         return new AxisName(processed);
