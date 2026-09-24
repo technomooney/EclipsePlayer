120bc880c1eed9a15a73d0a0e69ffee4b8ee6a01
Marty M. <github@moontronic.com>
2026-09-24 15:40:20 -0600

    Created static helper class for parsing timecode strings into a C# native TimeSpan type for easy processing.

--- EclipsePlayer.Funscript/Funscript.cs
+++ EclipsePlayer.Funscript/Funscript.cs
@@ -3,7 +3,17 @@
 
 namespace EclipsePlayer.Funscript;
 
-public class Funscript
+public static class Funscript
 {
+    internal static TimeSpan ParseTimeSpan(string rawTimeString)
+    {
+        var parts = rawTimeString.Split(':');
 
+        double seconds = double.Parse(parts[^1]); // ^ in the ^1 is the index-from-end operator.
+        int minutes = parts.Length > 1 ? int.Parse(parts[^2]) : 0; // ternary conditional operator saying if the len of the list is 2 or more, take the second to last index and parse as an int otherwise set to 0
+        int hours = parts.Length > 2 ? int.Parse(parts[^3]) : 0; // same as above, but len of 3
+
+        double totalSeconds = hours * 3600 + minutes * 60 + seconds;
+        return TimeSpan.FromSeconds(totalSeconds);
+    }
 }
